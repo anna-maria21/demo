d0f3bf24f46ca9aa784b21ab4a57d79ae0e8a631
anna-maria21 <strelchenko2010amg@gmail.com>
2022-11-08 16:17:34 +0200

    some changes in entity User

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
+
 @Document("user")
 @Data
 public class User {
@@ -12,4 +14,6 @@ public class User {
 
     String username;
     String password;
+
+    ArrayList<Case> cases;
 }
